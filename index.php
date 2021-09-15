d7c099dbf45328d599e8bccc4f4b2b699a91f014
andrin <you@example.com>
2021-09-15 12:11:11 +0200

    minor

--- index.php
+++ index.php
@@ -24,7 +24,6 @@ This as an example on how you can maka a php interface to Herby. Two tasks:
 
 <h2>Criar cartao de resposta:</h2>
 
-
 <?php
 $servername = "localhost:3306";
 $username = "root";
@@ -38,59 +37,80 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-echo "<br><br>";
-$sql = "SELECT student_id, student_name, class_id, class_name, school_name, school_id, year FROM Students";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo '<button onclick="getCrataoDeResposta(\''. $row["student_id"].'\', \''. $row["student_name"] .'\', \''
-    . $row["class_id"] .'\', \''. $row["class_name"] .'\', \''. $row["school_id"] .'\', \''. $row["school_name"] .'\', \''
-    .$row["year"] .'\')">Abaixar cartao de resposta</button>'. " - Nome: " . $row["student_name"]. " ". " - Classe: " . $row["class_name"]. " ". " - Escola: " . $row["school_name"]. " " . "<br><br>";
-  }
-} else {
-  echo "0 results";
+
+
+$age = array("Escola Heilo"=>"222", "Escola CAIC"=>"555");
+
+foreach($age as $x => $x_value) {
+
+
+  echo "<br><br>";
+  $sql = "SELECT student_id, student_name, class_id, class_name, school_name, school_id, year, city_id, city_name FROM Students WHERE school_id=$x_value;";
+  $result = $conn->query($sql);
+
+
+
+  echo '<button onclick="getCrataoDeResposta'.$x_value.'()">Abaixar cartao de resposta</button>'.' ' . $x  ;
+
+  echo '<script>
+          console.log("sdf");
+          function getCrataoDeResposta'.$x_value.'(){
+            var request = {
+              studentInformationList: [';
+              $ind = 0;
+              if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                   echo '
+                                 {studentId:"'.$row["student_id"].'",
+                                 student_name:"'. $row["student_name"] .'",
+                                 class_id:"'. $row["class_id"] .'",
+                                 class_name:"'. $row["class_name"] .'",
+                                 school_id:"'. $row["school_id"] .'",
+                                 school_name:"'. $row["school_name"] .'",
+                                 year:"'. $row["year"] .'",
+                                 city_id:"'. $row["city_id"] .'",
+                                 city_name:"'. $row["city_name"] .'"
+                               }';
+                  if ($ind!=$result->num_rows-1 ){
+                    echo ",";
+                  }
+
+                  $ind += 1;
+                }
+              }
+              else {
+                echo "0 results";
+              }
+  echo ']}
+          var xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+          console.log("this");
+          console.log(this);
+          var file =  new Blob([this.response], {type: "application/pdf"});
+          var fileURL = URL.createObjectURL(file);
+          window.open(fileURL);
+          }
+          };
+          xhttp.open("POST", "http://localhost:8082/getCartaoDeResposta", true);
+          xhttp.setRequestHeader("Content-type", "application/json");
+          xhttp.responseType = "arraybuffer";
+          console.log("http.responseType");
+          console.log(xhttp.responseType);
+          xhttp.send(JSON.stringify(request),{responseType: "arraybuffer"});
+          }
+  </script>
+  ';
 }
+
+
+
 $conn->close();
 ?>
 
 
 </div>
 
-
-
-<script>
-  console.log("sdf");
-  function getCrataoDeResposta(studentId, student_name, class_id, class_name, school_id, school_name, year){
-
-    var request = {
-      studentId:studentId,
-      student_name:student_name,
-      class_id:class_id,
-      class_name:class_name,
-      school_id:school_id,
-      school_name:school_name,
-      year:year
-    }
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log("this");
-        console.log(this);
-        var file =  new Blob([this.response], {type: 'application/pdf'});
-        var fileURL = URL.createObjectURL(file);
-        window.open(fileURL);
-      }
-    };
-    xhttp.open("POST", "http://localhost:8082/getCartaoDeResposta", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.responseType = "arraybuffer";
-    console.log("http.responseType");
-    console.log(xhttp.responseType);
-    xhttp.send(JSON.stringify(request),{responseType: 'arraybuffer'});
-  }
-
- </script>
 </body>
 </html>
--- setupDb.php
+++ setupDb.php
@@ -46,60 +46,74 @@ if ($conn->connect_error) {
 //
 
 
-$sql = "DROP TABLE  IF EXISTS Employees";
-$conn->query($sql)
-
+$sql = "DROP TABLE  IF EXISTS Students";
+if ($conn->query($sql) === TRUE) {
+  echo "Droped Table";
+} else {
+  echo "Message: " . $conn->error;
+}
+echo "<br>";
 
 // school_id,school_name,class_id,class_name,year,student_id,student_name,birth_date,gender
 // sql to create table
 $sql = "CREATE TABLE Students (
-        student_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        student_name VARCHAR(30) NOT NULL,
-        class_id  INT(6),
-        class_name VARCHAR(30) NOT NULL,
-        school_id  INT(6),
-        school_name VARCHAR(30) NOT NULL,
-        year INT(6),
-        city_id  INT(6),
-        city_name VARCHAR(30) NOT NULL
+student_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+student_name VARCHAR(30) NOT NULL,
+class_id  INT(6),
+class_name VARCHAR(30) NOT NULL,
+school_id  INT(6),
+school_name VARCHAR(30) NOT NULL,
+year INT(6),
+city_id  INT(6),
+city_name VARCHAR(30) NOT NULL
 )";
+//
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Message: " . $conn->error;
+}
+echo "<br>";
 
-// if ($conn->query($sql) === TRUE) {
-//   echo "Table MyGuests created successfully";
-// } else {
-//   echo "Message: " . $conn->error;
-// }
-// echo "<br>";
-//
-// // ==============================================================
-// // First delete all then insert Students
-// // goal is that there are alwais the 3 student "Carlo", "Juan", "Ana" in the DB
-//
-//
-// $sql = "DELETE FROM Students";
-// if ($conn->query($sql) === TRUE) {
-//   echo "Record deleted successfully";
-// } else {
-//   echo "Error deleting record: " . $conn->error;
-// }
-// echo "<br>";
-//
-//
-//
-// $sql = "INSERT INTO Students (student_name, class_id, class_name,school_id,school_name,year, city_name, city_id)
-// VALUES ('Carlo', 111,'2a',222 ,'Escola Heilo', 2 , "olinda",4 );";
-// $sql .= "INSERT INTO Students (student_name, class_id, class_name,school_id,school_name,year, city_name, city_id)
-// VALUES ('Juan', 111,'2a',222 ,'Escola Heilo', 2 , "olinda",4);";
-// $sql .= "INSERT INTO Students (student_name, class_id, class_name,school_id,school_name,year, city_name, city_id)
-// VALUES ('Ana', 444,'2b',333 ,'Escola Prof. Maria', 2, "Victoria De Conquista",4 );";
-//
-// if ($conn->multi_query($sql) === TRUE) {
-//   echo "New records created successfully";
-// } else {
-//   echo "Message: " . $sql . "<br>" . $conn->error;
-// }
-// $conn->close();
+// ==============================================================
+// First delete all then insert Students
+// goal is that there are alwais the 3 student "Carlo", "Juan", "Ana" in the DB
 
+
+$sql = "DELETE FROM Students";
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+echo "<br>";
+
+// $sql = "sdf"
+
+$sql = "INSERT INTO Students (student_name, class_id, class_name,school_id,school_name,year, city_id, city_name)
+        VALUES ('Carlo', 111,'2a',222 ,'Escola Heilo', 2 ,4, 'Olinda' );";
+
+
+
+$sql .= "INSERT INTO Students (student_name, class_id, class_name,school_id,school_name,year, city_id, city_name)
+                VALUES ('Juan', 111,'2a',222 ,'Escola Heilo', 2 ,4, 'Olinda' );";
+
+
+
+$sql .= "INSERT INTO Students (student_name, class_id, class_name,school_id,school_name,year, city_id, city_name)
+                VALUES ('Ana', 333,'2B',555 ,'Escola caic', 2 ,4, 'Baiha' );";
+
+
+echo $sql;
+
+if ($conn->multi_query($sql) === TRUE) {
+  echo "New records created successfully";
+} else {
+  echo "Message: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+
+echo "<br>";
 
 
 
@@ -113,30 +127,30 @@ $sql = "CREATE TABLE Students (
 // ===================================================================
 
 
-//
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Message: " . $conn->connect_error);
-// }
-//
-// // school_id,school_name,class_id,class_name,year,student_id,student_name,birth_date,gender
-// // sql to create table
-// $sql = "CREATE TABLE Results (
-// student_id INT(6) PRIMARY KEY,
-// test_name VARCHAR(30) NOT NULL,
-// first_field  INT(6),
-// second_field VARCHAR(30) NOT NULL,
-// )";
-//
-//
-// if ($conn->query($sql) === TRUE) {
-//   echo "Table MyGuests created successfully";
-// } else {
-//   echo "Message: " . $conn->error;
-// }
-// echo "<br>";
 
-// $conn->close();
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Message: " . $conn->connect_error);
+}
+
+// school_id,school_name,class_id,class_name,year,student_id,student_name,birth_date,gender
+// sql to create table
+$sql = "CREATE TABLE Results (
+    student_id INT(6) PRIMARY KEY,
+    test_name VARCHAR(30) NOT NULL,
+    first_field  INT(6),
+    second_field VARCHAR(30) NOT NULL
+)";
+
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Message: " . $conn->error;
+}
+echo "<br>";
+
+$conn->close();
 
 ?>
